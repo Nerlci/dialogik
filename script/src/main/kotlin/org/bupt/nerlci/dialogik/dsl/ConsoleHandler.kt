9b1bd94c42a867545081e9e7d0d4e282d2589649
Nerlci_ <nerlci.underline@outlook.com>
2023-12-14 18:24:56 +0800

    refactor ConsoleHandler

--- script/src/main/kotlin/org/bupt/nerlci/dialogik/dsl/ConsoleHandler.kt
+++ script/src/main/kotlin/org/bupt/nerlci/dialogik/dsl/ConsoleHandler.kt
@@ -2,6 +2,20 @@ package org.bupt.nerlci.dialogik.dsl
 
 class ConsoleHandler() : RobotHandler {
     override var conf: RobotConfig = RobotConfig()
+
+    fun handleMessage(msg: String): String {
+        val beforeAction = conf.getBeforeAction()
+        val afterAction = conf.getAfterAction()
+        val action = conf.getReceiveAction(msg)
+        val robotMessage = conf.getRobotMessage(msg)
+
+        val block = RobotActionBlock(robotMessage)
+        block.beforeAction()
+        block.action()
+        block.afterAction()
+        return block.response
+    }
+
     override fun start() {
         val welcomeActions = conf.getWelcomeActions()
         val block = RobotActionBlock()
@@ -9,7 +23,7 @@ class ConsoleHandler() : RobotHandler {
         println(block.response)
 
         while (true) {
-            val msg = readLine()
+            val msg = readlnOrNull()
             if (msg == null) {
                 val goodbyeActions = conf.getGoodbyeActions()
                 val block = RobotActionBlock()
@@ -18,17 +32,8 @@ class ConsoleHandler() : RobotHandler {
                 break
             }
 
-            val beforeAction = conf.getBeforeAction()
-            val afterAction = conf.getAfterAction()
-            val action = conf.getReceiveAction(msg)
-            val message = conf.getRobotMessage(msg)
-
-            val block = RobotActionBlock(message)
-            block.beforeAction()
-            block.action()
-            block.afterAction()
-            println(block.response)
-            break
+            val response = handleMessage(msg)
+            println(response)
         }
     }
 }
